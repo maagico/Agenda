dee046d10ec582ad64559e8449c285fdad3e7adb
Miguel Ángel <alvarez.garcia.miguel@gmail.com>
2022-06-18 15:47:35 +0200

    Clase limpiada

--- Agenda/src/main/java/es/agenda/controller/LoginController.java
+++ Agenda/src/main/java/es/agenda/controller/LoginController.java
@@ -2,8 +2,6 @@ package es.agenda.controller;
 
 import javax.servlet.http.HttpServletRequest;
 
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
